d93de33409acb333bcdd3910c1efea995eed3d48
Santiago Frega <fregasantiago@gmail.com>
2020-10-20 12:18:44 -0300

    First commit

---
+++ Assets/FloorMove.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mueve el piso
+        transform.position = new Vector3((float)(transform.position.x - 0.02), transform.position.y, transform.position.z);
+
+        if(transform.position.x < -10)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Stickman.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stickman : MonoBehaviour
+{
+    public Camera mainCamera;
+    Rigidbody2D rigidBody;
+
+    public float jumpForce = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y + jumpForce);
+        }
+
+        mainCamera.transform.position = new Vector3(transform.position.x + 5, transform.position.y + 1, mainCamera.transform.position.z);
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        
+
+
+    }
+}
