8f1ce788bdbbb9821d7aa23d3d26580a2472c8aa
Santiago Frega <fregasantiago@gmail.com>
2020-10-26 20:51:18 -0300

    Cajitaaaa

---
+++ Assets/spawn_obstacles.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_obstacles : MonoBehaviour
+{
+    public GameObject obstaculo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(obstaculo, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/wooden_crate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wooden_crate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3((float)(transform.position.x - 0.02), transform.position.y, transform.position.z);
+
+        if(transform.position.x < -10)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
