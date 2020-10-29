36150e0b2e4be380d899c50926e3c5c3589ec18c
Santiago Frega <fregasantiago@gmail.com>
2020-10-29 12:41:29 -0300

    """""""""Animaciones""""""""

---
+++ Assets/Scene_manager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene_manager : MonoBehaviour
+{
+    public float speed;
+    new Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.tag == "Obstacle")
+        {
+            rigidbody.velocity = new Vector2(-speed, 0);
+        }
+        else if (this.tag == "Floor")
+        {
+            transform.position = new Vector3(transform.position.x - speed*0.01f, transform.position.y, transform.position.z);
+
+        }
+        
+
+        if (transform.position.x < -10)
+        {
+            if(this.tag == "Obstacle")
+            {
+                Destroy(this.gameObject);
+            }
+            else if(this.tag == "Floor")
+            {
+                transform.position = new Vector3(10, transform.position.y, transform.position.z);
+
+            }
+
+        }
+
+    }
+}
--- Assets/Stickman.cs
+++ Assets/Stickman.cs
@@ -7,33 +7,45 @@ public class Stickman : MonoBehaviour
 {
     public Camera mainCamera;
     Rigidbody2D rigidBody;
+    Animator animator;
+    public Transform groundCheck;
+    public LayerMask groundMask;
 
+    bool isGrounded = false;
     public float jumpForce = 5;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        // Saltar (solo si estoy tocando el suelo)
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && isGrounded)
         {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y + jumpForce);
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
         }
 
         mainCamera.transform.position = new Vector3(transform.position.x + 5, transform.position.y + 1, mainCamera.transform.position.z);
+
+        //Control de las animaciones
+        animator.SetBool("IsRunning", isGrounded);
+
+        //Evita la rotacion por el collider tipo capsula
+        if(transform.rotation.z != 0)
+        {
+            transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, 0,0);
+        }
     }
 
 
     private void FixedUpdate()
     {
-
-        
-
-
+        //Chequeo del piso
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.02f, groundMask);
     }
 }
